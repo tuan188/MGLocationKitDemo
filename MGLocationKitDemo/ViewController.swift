39de6a72bc0f0c4533f6aa3f416aeee2339b0bfb
Tuan Truong <tuan188@gmail.com>
2017-03-31 11:15:21 +0700

    Init

---
+++ MGLocationKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MGLocationKitDemo
+//
+//  Created by Tuan Truong on 3/31/17.
+//  Copyright © 2017 Tuan Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
